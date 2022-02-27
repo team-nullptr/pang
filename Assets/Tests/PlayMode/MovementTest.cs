5773563d022a5fa0abfd1768a4b09945c666ee12
Pietrek14 <dawidekpe@gmail.com>
2022-02-27 03:22:24 +0100

    added a climbing test

--- Assets/Tests/PlayMode/MovementTest.cs
+++ Assets/Tests/PlayMode/MovementTest.cs
@@ -17,13 +17,16 @@ public class MovementTest
 		// Add the movement manager
 		MovementManager movementManager = player.AddComponent<MovementManager>();
 
-		// Move right
-		movementManager.Move(Vector2.right);
+		// Move in the direction
+		movementManager.Move(direction);
 
         yield return new WaitForSeconds(1f);
 
-		// Check if the player has moved right
-		Assert.Greater(player.transform.position.x, 0f);
+		// Check if the player has moved in the given direction
+		if(direction.x > 0)
+			Assert.Greater(player.transform.position.x, 0f);
+		else
+			Assert.Less(player.transform.position.x, 0f);
 	}
 
     [UnityTest]
@@ -37,4 +40,34 @@ public class MovementTest
     {
 		yield return Move(Vector2.left);
     }
+
+	[UnityTest]
+	public IEnumerator Climb() {
+		// Instantiate a mock player
+		GameObject player = new GameObject();
+
+		// Add components required by movement manager
+		player.AddComponent<Rigidbody2D>();
+		player.AddComponent<CapsuleCollider2D>();
+
+		// Add the movement manager
+		MovementManager movementManager = player.AddComponent<MovementManager>();
+
+		// Create a mock ladder
+		GameObject ladder = new GameObject();
+		BoxCollider2D ladderCollider = ladder.AddComponent<BoxCollider2D>();
+		ladderCollider.size = new Vector2(5f, 1f);
+		ladder.tag = "Ladder";
+
+		// Set the player position to 0, 0
+		player.transform.position = new Vector3(0f, 0f, 0f);
+
+		// Move in the direction
+		movementManager.Move(Vector2.up);
+
+		yield return new WaitForSeconds(1f);
+
+		// Check if the player has climbed up
+		Assert.Greater(player.transform.position.y, 0f);
+	}
 }
